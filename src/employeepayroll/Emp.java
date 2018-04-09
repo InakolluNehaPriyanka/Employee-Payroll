c16ef8bc82cd7f2c10f9f11009bc4cab7f49140b
InakolluNehaPriyanka <31779044+InakolluNehaPriyanka@users.noreply.github.com>
2018-04-09 12:12:47 +0530

    other functionalities

---
+++ src/employeepayroll/Emp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeepayroll;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Emp {
+    
+    public static int empId;
+    public static String empname;
+}
+  
---
+++ src/employeepayroll/SimpleDataFormat.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeepayroll;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LENOVO
+ */
+class SimpleDataFormat {
+
+    SimpleDataFormat(String hHmmss) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String format(Date d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
